33f88641a4a5949e6d8de3526128095add325fbf
Anyoh98 <fonachuanyoh@yahoo.com>
2023-12-24 16:58:18 -0500

    tetsing

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from pathlib import Path
+from fabric import task
+from fabric.api import local
+
+@task
+def do_pack():
+    if not Path("verisons").is_dir():
+        local("mkdir -p versions")
+    current_date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = "versions/web_static_{}.tgz".format(current_date)
+    result = local("tar -czvf {} web_static".format(archive_name))
+    if result.succeeded:
+        return archive_name
+    else:
+        return None
