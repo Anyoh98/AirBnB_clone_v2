514044ff7c73fcdf49c879ed776c07c89703fbf6
Anyoh98 <fonachuanyoh@yahoo.com>
2024-01-01 21:43:10 -0500

    rename

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
-starts a Flask web application
+flask applictaion will start from here
 """
 
 from flask import Flask
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    """returns Hello HBNB!"""
+    """it will return the corrresponding anser"""
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
