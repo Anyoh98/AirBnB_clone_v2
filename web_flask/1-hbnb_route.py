0087bc2096dacff07d70ce036e6d636bdaff2d0f
Anyoh98 <fonachuanyoh@yahoo.com>
2024-01-01 21:45:36 -0500

    rename

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+flask applictaion startes
 """
 
 from flask import Flask
@@ -9,13 +9,13 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    """returns Hello HBNB!"""
+    """return the given answer"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
+    """Return the correct answeer"""
     return 'HBNB'
 
 if __name__ == '__main__':
