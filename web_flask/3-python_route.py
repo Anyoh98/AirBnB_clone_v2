34e6f536182b1c583c606de984e57816d2a3ee01
Anyoh98 <fonachuanyoh@yahoo.com>
2024-01-01 21:51:48 -0500

    rename

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+A simple wbe flask applictaion
 """
 
 from flask import Flask
@@ -9,26 +9,26 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    """returns Hello HBNB!"""
+    """return the given answzer"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
+    """return the given answer"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
+    """Dsiplay c follewd by cale"""
     return 'C ' + text.replace('_', ' ')
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def pythoniscool(text='is cool'):
-    """display “Python ”, followed by the value of the text variable"""
+    """Dsipya Python"""
     return 'Python ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
