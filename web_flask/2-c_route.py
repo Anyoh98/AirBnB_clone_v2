4654330b8d899db815f9e808a58a0ca4d201b8ab
Anyoh98 <fonachuanyoh@yahoo.com>
2024-01-01 21:50:26 -0500

    rename

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+A imple flask web application
 """
 
 from flask import Flask
@@ -9,19 +9,22 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    """returns Hello HBNB!"""
+    """return the Giveb answer"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
+    """return the given answer"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
+    """
+    Displays “C ” followed by the value of the text variable.
+    Replaces underscores with spaces in the text.
+    """
     return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
